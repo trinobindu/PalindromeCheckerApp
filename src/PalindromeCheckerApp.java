e276ca6c56466ae570d063d8a65a847237a539ff
trinobindu <tdas24388@gmail.com>
2026-02-25 09:36:59 +0530

    Final cleanup after UC2 merge

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,31 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("================================");
         System.out.println("   WELCOME TO PALINDROME APP   ");
         System.out.println("================================");
 
-        String word = "madam";
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
+        String cleaned = word.replaceAll("\\s+", "").toLowerCase();
         String reversed = "";
 
-        for(int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for(int i = cleaned.length() - 1; i >= 0; i--) {
+            reversed = reversed + cleaned.charAt(i);
         }
 
-        if(word.equals(reversed)) {
+        if(cleaned.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
         }
+
+        scanner.close();
     }
 }
