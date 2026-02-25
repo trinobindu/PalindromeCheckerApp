daedc4b0b116ac437a6c91421c89d6dd6fa41db5
trinobindu <tdas24388@gmail.com>
2026-02-25 09:03:18 +0530

    UC3: User input palindrome with space handling

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,5 +6,17 @@ public class PalindromeCheckerApp {
         System.out.println("   WELCOME TO PALINDROME APP   ");
         System.out.println("================================");
 
+        String word = "madam";
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
