7931c87d74ec4b27fd592986dc62145f55597d10
trinobindu <tdas24388@gmail.com>
2026-03-05 10:22:18 +0530

    UC1: Basic reverse palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-
+    public static boolean checkPalindromeBasic(String input) {
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        return input.equals(reversed);
+    }
     public static void main(String[] args) {
 
         System.out.println("================================");
